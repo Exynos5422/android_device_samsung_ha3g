77044e5535c44618069d5531397089b92c12e9db
Adithya <adithyadinesh96@gmail.com>
2017-02-09 15:38:12 +0530

    RIL : override switchToRadioState(RadioState) ,Overlay:Depricated over in AOSP

--- ril/telephony/java/com/android/internal/telephony/SlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/SlteRIL.java
@@ -200,12 +200,12 @@ public class SlteRIL extends RIL {
 
         send(rr);
     }
-
-    private void switchToRadioState(RadioState newState) {
-        setRadioState(newState);
+	@Override
+    protected void switchToRadioState(RadioState newState) {
+        super.switchToRadioState(newState);
 
         if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
-            getIccCardStatus(mPendingGetSimStatus);
+            super.getIccCardStatus(mPendingGetSimStatus);
             mPendingGetSimStatus = null;
         }
     }
